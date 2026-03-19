3de12c8b34113774075823fcce21f62c4e8e1175
ArcanjoxDoug <douglasbatistaarcanjo@gmail.com>
2026-03-19 10:13:00 -0300

    Update auth.js

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,1 +1,15 @@
+const jwt = require('jsonwebtoken');
 
+module.exports = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader)
+    return res.status(401).json({ erro: 'Token não fornecido' });
+
+  const token = authHeader.split(' ')[1];
+  try {
+    req.admin = jwt.verify(token, process.env.JWT_SECRET);
+    next();
+  } catch {
+    res.status(401).json({ erro: 'Token inválido ou expirado' });
+  }
+};
